335e03961fb79a3c372c8cd2a70db3bea13f3786
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-19 17:50:14 +0100

    Trying to solve build error

--- config.rb
+++ config.rb
@@ -55,12 +55,12 @@ configure :development do
   activate :livereload
 end
 
-require 'extensions/build_cleaner'
-
-configure :build do
-  activate :relative_assets
-  activate :build_cleaner
-end
+# require 'extensions/build_cleaner'
+#
+# configure :build do
+#   activate :relative_assets
+#   activate :build_cleaner
+# end
 
 activate :deploy do |deploy|
   deploy.build_before = true
