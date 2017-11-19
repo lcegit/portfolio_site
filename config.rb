ea26f41b73385c39a80983c2bd4284ed55951e4a
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-19 18:04:50 +0100

    Fixing deploy error, adding a build

--- config.rb
+++ config.rb
@@ -108,18 +108,18 @@ ignore "javascripts/script.js"
 
 # Build-specific configuration
 configure :build do
-  add_import_path "node_modules/foundation-sites/scss"
+  # add_import_path "node_modules/foundation-sites/scss"
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
